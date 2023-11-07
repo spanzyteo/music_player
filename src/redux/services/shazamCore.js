b370765092b868a83140cc0f2a79355785dec47c
spanzy teo <spanzytheo@gmail.com>
2023-11-07 03:48:11 +0100

    music-player
    description

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam.p.rapidapi.com',
+    prepareHeaders: (headers) => {
+      headers.set(
+        'X-RapidAPI-Key',
+        '721621ae4fmshf1c35f32b90e3a9p14cb4ajsn6ea9cf84441b'
+      )
+
+      return headers
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/track' }),
+  }),
+})
+
+export const { useGetTopChartsQuery } = shazamCoreApi
