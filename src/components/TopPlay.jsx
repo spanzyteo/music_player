706759fbbf96671ca8147b635ba9196bf4d507be
spanzy teo <spanzytheo@gmail.com>
2023-11-08 23:47:28 +0100

    added the sidebar component for desktop and mobile view
    description

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,5 +1,19 @@
-const TopPlay = () => (
-  <div>TopPlay</div>
-);
+import { useEffect, useRef } from 'react'
+import { Link } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { FreeMode } from 'swiper'
 
-export default TopPlay;
+import PlayPause from './PlayPause'
+import { playPause, setActiveSong } from '../redux/features/playerSlice'
+import { useGetTopChartsQuery } from '../redux/services/shazamCore'
+
+import 'swiper/css'
+import 'swiper/css/free-mode'
+
+const TopPlay = () => {
+  const dispatch = useDispatch()
+  const { activeSong, isPlaying } = useSelector((state) => state.player)
+}
+
+export default TopPlay
