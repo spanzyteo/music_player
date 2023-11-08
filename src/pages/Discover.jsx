3df6dbae75ff0e6e6238770aaec64801caddbd6c
spanzy teo <spanzytheo@gmail.com>
2023-11-08 20:05:15 +0100

    added playing functionality
    description

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -6,6 +6,8 @@ import { genres } from '../assets/constants'
 import { useGetTopChartsQuery } from '../redux/services/shazamCore'
 
 const Discover = () => {
+  const dispatch = useDispatch()
+  const { activeSong, isPlaying } = useSelector((state) => state.player)
   const { data, isFetching, error } = useGetTopChartsQuery()
   const genreTitle = 'Pop'
 
@@ -36,7 +38,14 @@ const Discover = () => {
 
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
         {data?.tracks.map((song, i) => (
-          <SongCard key={song.key} song={song} i={i} />
+          <SongCard
+            key={song.key}
+            song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
+            i={i}
+          />
         ))}
       </div>
     </div>
